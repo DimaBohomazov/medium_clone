6302cdb31ec26c96da765e3c0915c4525d75ab88
Bohomazov Dmitriy <bohomazov.dmitriy@gmail.com>
2020-06-01 20:16:21 +0300

    create useLocalStorage.js

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,8 @@
+import {useEffect, useState} from "react";
+
+export default (key, initialValue = '') => {
+  const [value,setValue] = useState(() => localStorage.getItem(key) || initialValue)
+
+  useEffect(()=> localStorage.setItem(key, value),[value])
+  return [value, setValue]
+}
--- src/pages/authentication/index.js
+++ src/pages/authentication/index.js
@@ -1,40 +1,69 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import {Link, Redirect} from 'react-router-dom'
 import useFetch from "../../hooks/useFetch";
+import useLocalStorage from "../../hooks/useLocalStorage";
 
-const Authentication = () => {
+const Authentication = props => {
+  const isLogin = props.match.path === '/login'
+  const pageTitle = isLogin ? 'Sing In' : 'Sing Up'
+  const descriptionLink = isLogin ? '/register' : '/login'
+  const descriptionText = isLogin ? 'Need an account?' : 'Have an account?'
+  const apiUrl = isLogin ? '/users/login' : '/users'
+
+  const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  // const [isSubmitting, setIsSubmitting] = useState(false)
-  const [{isLoading, response, error}, doFetch] = useFetch('/users/login')
-
-  console.log(response, isLoading, error)
+  const [isSuccessSubmit, setIsSuccessSubmit] = useState(false)
+  const [{isLoading, response, error}, doFetch] = useFetch(apiUrl)
+  const [token, setToken] = useLocalStorage('token')
 
   const handleSubmit = (event) => {
     event.preventDefault()
+    const user = isLogin ? {email, password} : {email, password, username}
     doFetch({
       method: 'post',
       data: {
-        user: {
-          email: 'qq@gmail.com',
-          password: '123'
-        }
+        user
       }
     })
   }
+
+  useEffect(() => {
+    if(!response){
+      return
+    }
+    setToken(response.user.token)
+    setIsSuccessSubmit(true)
+  }, [response])
+
+  if (isSuccessSubmit){
+    return <Redirect to='/' />
+  }
+  console.log('token', token)
   return (
     <div className='auth-page'>
       <div className="container page">
         <div className="row">
           <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className='text-xs-center'>Login</h1>
+            <h1 className='text-xs-center'>{pageTitle}</h1>
             <p className="text-xs-center">
-              <Link to='/register'>
-                Need an account?
+              <Link to={descriptionLink}>
+                {descriptionText}
               </Link>
             </p>
             <form onSubmit={handleSubmit}>
               <fieldset>
+                {!isLogin && (
+                  <fieldset className="form-group">
+                    <input
+                      className='form-control form-control-lg'
+                      type="text"
+                      placeholder='Username'
+                      value={username}
+                      onChange={e => setUsername(e.target.value)}
+                    />
+                  </fieldset>
+                )}
                 <fieldset className="form-group">
                   <input
                     className='form-control form-control-lg'
@@ -58,7 +87,7 @@ const Authentication = () => {
                   type='submit'
                   disabled={isLoading}
                 >
-                  Sing In
+                  {pageTitle}
                 </button>
               </fieldset>
             </form>
